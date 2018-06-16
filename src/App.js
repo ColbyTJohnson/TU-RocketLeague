e2bfcca44005cb92f97f318ee11d7e67b3a7ec25
Colby Johnson <cjohnson@matchbooklearning.com>
2018-06-16 18:39:58 -0400

    updates folder structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Footer from './components/Footer';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-                <footer className="App-footer">
-                    <h4>Made with React</h4>
-                    <img src={logo} className="App-logo" alt="logo"/>
-                </footer>
+            <div>
+                <div className="App">
+                    <p className="App-intro">
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                    </p>
+                </div>
+                <Footer />
             </div>
         );
     }
