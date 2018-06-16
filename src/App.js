2018c4423b44ebc3fbaf1b7f3f955b462292934f
Colby Johnson <cjohnson@matchbooklearning.com>
2018-06-16 18:57:05 -0400

    updated .gitignore and removed /.idea folder from repo

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Footer from './components/Footer';
 class App extends Component {
     render() {
         return (
-            <div>
-                <div className="App">
-                    <p className="App-intro">
-                        To get started, edit <code>src/App.js</code> and save to reload.
+            <div className="App">
+                <div>
+                    <p>
+                        Welcome to TU | Rocket League's home page!
                     </p>
                 </div>
                 <Footer />
