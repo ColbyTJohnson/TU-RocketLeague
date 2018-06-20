20c1b758f369daa77dd04ee6a1428b2118c526ed
Colby Johnson <coldrevolutionsent@gmail.com>
2018-06-20 12:18:18 -0400

    Add image assets and RocketLeagueStats-required branding

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import RLS from '../../assets/rls_partner_horizontal_large.png';
 import './Header.css';
 
 const header = () => (
     <div className="Header">
         <h2>TU | Rocket League</h2>
+        <img src={RLS}  className="RLS" alt="RocketLeagueStats" onClick={() => {window.open('https://rocketleaguestats.com/', '_blank')}}></img>
     </div>
 );
 
---
+++ src/components/Player/PlayerCard/PlayerCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './PlayerCard.css';
+
+const playerCard = () => (
+    <div className="Card">
+        <h2>TU | Rocket League</h2>
+    </div>
+);
+
+export default playerCard;
