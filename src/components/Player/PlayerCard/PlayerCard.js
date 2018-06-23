5a2128c46f72fa491741d83ab512e6e699b1057e
Colby Johnson <coldrevolutionsent@gmail.com>
2018-06-22 20:24:16 -0400

    Add player profile picture, Remove unnecessary pictures, Update color scheme

--- src/components/Player/PlayerCard/PlayerCard.js
+++ src/components/Player/PlayerCard/PlayerCard.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 import './PlayerCard.css';
-import perino from '../../../assets/perino.jpg';
 
 const playerCard = (props) => (
     <div className="Card">
         <div className="Player-header">
-            <img src={perino} alt="perino's ugly car" className="Photo" />
+            <img src={props.img} alt="perino's ugly car" className="Photo" />
             <div className="Player-name">
                 <p className="Name">{props.name}</p>
             </div>
--- src/containers/CardView/CardView.js
+++ src/containers/CardView/CardView.js
@@ -3,11 +3,22 @@ import React from 'react';
 import PlayerCard from '../../components/Player/PlayerCard/PlayerCard';
 import './CardView.css';
 
+import cierex from '../../assets/cie.jpg';
+import sadboy from '../../assets/sad.jpg';
+import zenlos from '../../assets/zen.jpg';
+
+
 const cardView = () => (
     <div className="CardView">
-        <PlayerCard name={"Cierex"} />
-        <PlayerCard name={"s a d b o y"} />
-        <PlayerCard name={"Zenlos"} />
+        <PlayerCard
+            name={"Cierex"}
+            img={cierex} />
+        <PlayerCard
+            name={"s a d b o y"}
+            img={sadboy} />
+        <PlayerCard
+            name={"Zenlos"}
+            img={zenlos} />
     </div>
 );
 
