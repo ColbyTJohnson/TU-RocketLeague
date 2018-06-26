91f55c7405af1b3132c43e3d2bf982003d7382b3
Colby Johnson <coldrevolutionsent@gmail.com>
2018-06-26 16:24:25 -0400

    Update .gitignore to exclude API key file

--- src/containers/CardView/CardView.js
+++ src/containers/CardView/CardView.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
+import rls from 'rls-api';
 
 import PlayerCard from '../../components/Player/PlayerCard/PlayerCard';
+import Keys from '../../store/Keys';
 import './CardView.css';
 
 import cierex from '../../assets/cie.jpg';
@@ -10,21 +12,71 @@ import zenlos from '../../assets/zen.jpg';
 import ranks from '../../store/Ranks';
 
 
-const cardView = () => (
-    <div className="CardView">
-        <PlayerCard
-            name={"Cierex"}
-            img={cierex}
-            rank={ranks.bronze_1} />
-        <PlayerCard
-            name={"s a d b o y"}
-            img={sadboy}
-            rank={ranks.gc} />
-        <PlayerCard
-            name={"Zenlos"}
-            img={zenlos}
-            rank={ranks.champ_2} />
-    </div>
-);
+class CardView extends Component {
+    constructor(props) {
+        super(props);
 
-export default cardView;
+        this.state = {
+            sadboyGoals: 0,
+            sadboyAssists: 0,
+            sadboySaves: 0,
+        };
+
+        this.setStats = this.setStats.bind(this);
+    }
+
+    setStats (goals, assists, saves) {
+        this.setState({
+            sadboyGoals: goals,
+            sadboyAssists: assists,
+            sadboySaves: saves,
+        });
+
+        console.log(this.state.goals);
+        console.log(this.state.assists);
+        console.log(this.state.saves);
+    }
+
+    componentDidMount () {
+        // let client = new rls.Client({
+        //     token: Keys.rlsToken
+        // });
+        //
+        // client.getPlayer("76561198168703285", rls.platforms.STEAM, function(status, data){
+        //     if(status === 200){
+        //         console.log("-- Player Data:");
+        //         console.log("   Display name: " + data.displayName);
+        //         console.log("   Goals: " + data.stats.goals);
+        //         console.log("   Assists: " + data.stats.assists);
+        //         console.log("   Saves: " + data.stats.saves);
+        //         let goals = data.stats.goals;
+        //         let assists = data.stats.assists;
+        //         let saves = data.stats.saves;
+        //         this.setStats(goals, assists, saves);
+        //     } else {
+        //         console.log("-- getPlayer failed: " + status);
+        //     }
+        // });
+    }
+
+    render () {
+        return (
+            <div className="CardView">
+                <PlayerCard
+                    name={"Cierex"}
+                    img={cierex}
+                    rank={ranks.bronze_1} />
+                <PlayerCard
+                    name={"s a d b o y"}
+                    img={sadboy}
+                    rank={ranks.gc} />
+                <PlayerCard
+                    name={"Zenlos"}
+                    img={zenlos}
+                    rank={ranks.champ_2} />
+            </div>
+        );
+    }
+}
+
+export default CardView;
